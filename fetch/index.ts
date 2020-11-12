15059d69642d5cdbd16e3d111d7f0fcc7d097cfd
Nicolas <nicolas@lebongustave.fr>
2020-11-12 18:20:42 +0100

    Changed VERCEL_URL to URL

--- fetch/index.ts
+++ fetch/index.ts
@@ -1,7 +1,7 @@
 import { LoginInterface } from '../pages/login';
 import { AddressType, CustomerType, ReadingType } from '../types';
 
-const { VERCEL_URL = 'http://localhost:3000' } = process.env;
+const { URL = 'http://localhost:3000' } = process.env;
 
 export interface CreateReadingType {
   reading: ReadingType;
@@ -19,37 +19,37 @@ export interface UpdateCustomerType {
 }
 
 export const createAddress = ({ address, id }: CreateAddressType) =>
-  fetch(`${VERCEL_URL}/api/addresses?id=${id}`, {
+  fetch(`${URL}/api/addresses?id=${id}`, {
     body: JSON.stringify(address),
     method: 'POST',
   }).then((res) => res.json());
 
 export const createCustomer = (customer: CustomerType) =>
-  fetch(`${VERCEL_URL}/api/customers`, {
+  fetch(`${URL}/api/customers`, {
     body: JSON.stringify(customer),
     method: 'POST',
   }).then((res) => res.json());
 
 export const createReading = ({ reading, id }: CreateReadingType) =>
-  fetch(`${VERCEL_URL}/api/readings`, {
+  fetch(`${URL}/api/readings`, {
     body: JSON.stringify({ ...reading, customerId: id }),
     method: 'POST',
   }).then((res) => res.json());
 
 export const getAddresses = (id: string) =>
-  fetch(`${VERCEL_URL}/api/addresses?id=${id}`).then((res) => res.json());
+  fetch(`${URL}/api/addresses?id=${id}`).then((res) => res.json());
 
 export const getCustomer = (id: string) =>
-  fetch(`${VERCEL_URL}/api/customers?id=${id}`).then((res) => res.json());
+  fetch(`${URL}/api/customers?id=${id}`).then((res) => res.json());
 
 export const login = (data: LoginInterface) =>
-  fetch(`${VERCEL_URL}/api/login`, {
+  fetch(`${URL}/api/login`, {
     body: JSON.stringify(data),
     method: 'POST',
   }).then((res) => res.json());
 
 export const updateCustomer = ({ customer, id }: UpdateCustomerType) =>
-  fetch(`${VERCEL_URL}/api/customers?id=${id}`, {
+  fetch(`${URL}/api/customers?id=${id}`, {
     body: JSON.stringify(customer),
     method: 'PUT',
   }).then((res) => res.json());
