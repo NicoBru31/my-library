1de8ed261f5a820cc71ff03068626fcab376c3f2
Nicolas <nicolas@lebongustave.fr>
2020-11-13 08:44:01 +0100

    Remove spaces in env var

--- fetch/index.ts
+++ fetch/index.ts
@@ -19,37 +19,41 @@ export interface UpdateCustomerType {
 }
 
 export const createAddress = ({ address, id }: CreateAddressType) =>
-  fetch(`${NEXT_PUBLIC_URL}/api/addresses?id=${id}`, {
+  fetch(`${NEXT_PUBLIC_URL.replace(/ /g, '')}/api/addresses?id=${id}`, {
     body: JSON.stringify(address),
     method: 'POST',
   }).then((res) => res.json());
 
 export const createCustomer = (customer: CustomerType) =>
-  fetch(`${NEXT_PUBLIC_URL}/api/customers`, {
+  fetch(`${NEXT_PUBLIC_URL.replace(/ /g, '')}/api/customers`, {
     body: JSON.stringify(customer),
     method: 'POST',
   }).then((res) => res.json());
 
 export const createReading = ({ reading, id }: CreateReadingType) =>
-  fetch(`${NEXT_PUBLIC_URL}/api/readings`, {
+  fetch(`${NEXT_PUBLIC_URL.replace(/ /g, '')}/api/readings`, {
     body: JSON.stringify({ ...reading, customerId: id }),
     method: 'POST',
   }).then((res) => res.json());
 
 export const getAddresses = (id: string) =>
-  fetch(`${NEXT_PUBLIC_URL}/api/addresses?id=${id}`).then((res) => res.json());
+  fetch(
+    `${NEXT_PUBLIC_URL.replace(/ /g, '')}/api/addresses?id=${id}`,
+  ).then((res) => res.json());
 
 export const getCustomer = (id: string) =>
-  fetch(`${NEXT_PUBLIC_URL}/api/customers?id=${id}`).then((res) => res.json());
+  fetch(
+    `${NEXT_PUBLIC_URL.replace(/ /g, '')}/api/customers?id=${id}`,
+  ).then((res) => res.json());
 
 export const login = (data: LoginInterface) =>
-  fetch(`${NEXT_PUBLIC_URL}/api/login`, {
+  fetch(`${NEXT_PUBLIC_URL.replace(/ /g, '')}/api/login`, {
     body: JSON.stringify(data),
     method: 'POST',
   }).then((res) => res.json());
 
 export const updateCustomer = ({ customer, id }: UpdateCustomerType) =>
-  fetch(`${NEXT_PUBLIC_URL}/api/customers?id=${id}`, {
+  fetch(`${NEXT_PUBLIC_URL.replace(/ /g, '')}/api/customers?id=${id}`, {
     body: JSON.stringify(customer),
     method: 'PUT',
   }).then((res) => res.json());
